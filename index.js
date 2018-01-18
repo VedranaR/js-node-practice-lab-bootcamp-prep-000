c68b4035648d0d612542366af142e9adf59d3ee5
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-18 14:01:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // write your code below!
 
 var name = "Vedrana";
-var height = "74";
+var height = 170;
 
 console.log("Name:");
 console.log(name);
 
 console.log("Height:");
-console.log(height);
+console.log(height + 1);
 
 
-console.log(parseInt(height) + 1 );
